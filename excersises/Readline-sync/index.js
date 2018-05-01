7e0dbbed0aed2005d058c844497e7e124267cf65
drt272728 <drt2728@icloud.com>
2018-05-01 10:02:32 -0600

    submitting daily planet editor

---
+++ excersises/Readline-sync/index.js
@@ -0,0 +1,33 @@
+var ask = require("readline-sync");
+
+console.log("");
+
+var firstName = ask.question("What is your first name?");
+
+console.log (firstName)
+
+var whatIsYourAge = ask.question("How old are you?");
+
+console.log (whatIsYourAge)
+
+var favoriteColor = ask.question("What is your favorite color?");
+
+console.log (favoriteColor)
+
+var favoriteMovie = ask.question("What is your favorite movie?");
+
+console.log (favoriteMovie)
+
+var whereWereYouBorn = ask.question("Where were you born?");
+
+console.log (whereWereYouBorn)
+
+var whereDidYouGoToHighSchool = ask.question("Where did you go to high school?");
+
+console.log (whereDidYouGoToHighSchool)
+
+var whatIsYourFavoriteRestaurant = ask.question("whatIsYourFavoriteResaturant?")
+
+console.log (whatIsYourFavoriteRestaurant)
+
+ 
