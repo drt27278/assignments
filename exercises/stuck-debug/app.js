e379d92f5e1b300fd1dbb06b83fe85fac889b349
DIllon <drt2728@icloud.com>
2018-08-01 14:27:33 -0600

    submit stuck debug

--- exercises/stuck-debug/app.js
+++ exercises/stuck-debug/app.js
@@ -1,15 +1,21 @@
-var ask = require("readline-sync");
+var ask = require('readline-sync');
 
 var options = ['pick flowers', 'shoot guns', 'fight bears'];
 var option = 'undecided';
 
-while(option != 'fight bears'){
-  ask.keyInSelect(options, "What would you like to do today?: ");
-  if(option = 'pick flowers'){
+while (option !== 'fight bears') {
+  option = ask.keyInSelect(options, "What would you like to do today?: ");
+  if (options[option] === 'pick flowers'){
     console.log("You pick some flowers. You make a bouquet.");
-  } else {
-    console.log("You shoot guns.");
+  }else if(options[option] ==='shoot guns'){
+    console.log('you shoot guns');
+
+  }else if(options[option] ==='fight bears'){
+    option = 'fight bears';
+  }else {
+    console.log("Please select an option");
   }
+
 }
 
 console.log("You fought a bear and got beat up!");
