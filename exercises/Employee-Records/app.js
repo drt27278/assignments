0832c0784b1144608042fed4eea2395e9f81ac0a
DIllon <drt2728@icloud.com>
2018-08-02 10:48:56 -0600

    added read me to exercises

---
+++ exercises/Employee-Records/app.js
@@ -0,0 +1,10 @@
+var employees = [];
+
+
+function names (name, title, salary, status){
+    this.name = name;
+    this.title = title;
+    this.salary = salary;
+    this.status = status;
+}
+console.log();
